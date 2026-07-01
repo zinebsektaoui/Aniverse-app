dafbdd3fe80ab36f1adff4a18c76604605a28001
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-07-01 11:59:07 +0100

    les infos de l'application

---
+++ src/constants/appInfo.js
@@ -0,0 +1,16 @@
+import hero from "../assets/hero.jpg";
+
+export const appInfo = {
+  badge: "Discover Your Next Obsession",
+  title: {
+    first: "Welcome to",
+    highlighted: "AniVerse",
+  },
+  description:
+    "Explore tens of thousands of anime, follow seasonal releases, view detailed profiles of your favorite characters, and organize your ultimate watchlist with custom scores, notes, and progress logs.",
+  cta: {
+    text: "Explore Anime Now",
+    path: "/anime",
+  },
+  pic: hero
+};
