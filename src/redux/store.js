e0a5c19b2ea04d742d040364da78f5655b347ecc
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-07-02 16:08:07 +0100

    l'ajout de favorite reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import animeReducer from "../redux/slices/animeSlice"
+import favoriteReducer from "../redux/slices/favoriteSlice"
 
 export const store = configureStore({
   reducer: {
     anime: animeReducer,
+    favorite: favoriteReducer
   },
 });
