8a36321836b9c3e6cf2ffcedea4e23873bc0a2b9
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-07-02 16:11:37 +0100

    updates

--- src/components/anime/AnimeCard.jsx
+++ src/components/anime/AnimeCard.jsx
@@ -1,25 +1,33 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Star } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 import Spinner from "../layouts/Spinner";
 import ErrorState from "../layouts/ErrorState";
 import SearchBar from "./SearchBar";
 
 function AnimeCard() {
-  const { animList, error, loading } = useSelector((state) => state.anime);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { selectedAnime, animList, error, loading } = useSelector(
+    (state) => state.anime
+  );
 
   if (loading) return <Spinner />;
   if (error) return <ErrorState message={error.message} />;
   if (animList?.length === 0)
     return <div className="text-slate-400 px-6 py-10">No anime found.</div>;
+  const handleClick = (anime) => {
+    navigate(`/anime/${anime.mal_id}/full`);
+  };
 
   return (
     <section className="w-full px-4 md:px-8 lg:px-12">
-      <SearchBar />
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {animList.map((anime) => (
           <div
+            onClick={() => handleClick(anime)}
             key={anime.mal_id}
             className="group relative bg-[#0f1420] rounded-2xl overflow-hidden border border-white/5 hover:border-orange-500/30 transition-all cursor-pointer"
           >
