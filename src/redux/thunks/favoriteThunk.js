d86612ffbceda28fce5724d66d236590a76872b7
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-07-03 09:43:01 +0100

    function to delete an anime from favorite page

--- src/redux/thunks/favoriteThunk.js
+++ src/redux/thunks/favoriteThunk.js
@@ -2,29 +2,41 @@ import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
 import jsonServer from "../../services/jsonServer";
 
 export const addToFav = createAsyncThunk(
-    "/anime/fav",
-    async(anime, {rejectWithValue}) => {
-        try {
-            const existing = await jsonServer.get(`favorites?mal_id=${anime.mal_id}`)
-            if (existing.data.length > 0) {
-                return rejectWithValue("Anime is already in favorites")
-            }
-            const res = await jsonServer.post("favorites", anime)
-            return res.data
-        } catch (error) {
-            return rejectWithValue(error.response.data)
-        }
+  "/anime/fav",
+  async (anime, { rejectWithValue }) => {
+    try {
+      const existing = await jsonServer.get(`favorites?mal_id=${anime.mal_id}`);
+      if (existing.data.length > 0) {
+        return rejectWithValue("Anime is already in favorites");
+      }
+      const res = await jsonServer.post("favorites", anime);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-)
+  },
+);
 
 export const getFav = createAsyncThunk(
-    "/favorites",
-    async(_, {rejectWithValue}) => {
-        try {
-            const res = await jsonServer.get("favorites")
-            return res.data
-        } catch (error) {
-            return rejectWithValue(error.response.data)
-        }
+  "/favorites",
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await jsonServer.get("favorites");
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
-)
+  },
+);
+
+export const removeFromFav = createAsyncThunk(
+  "/favorites/remove",
+  async (anime, { rejectWithValue }) => {
+    try {
+      const response = await jsonServer.delete(`favorites/${anime.id}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
