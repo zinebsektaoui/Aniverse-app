54da9a7a9bc3a530b488ca626846625ac50e87cc
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-07-03 17:04:42 +0100

    l'ajout de reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import animeReducer from "../redux/slices/animeSlice"
-import favoriteReducer from "../redux/slices/favoriteSlice"
+import animeReducer from "../redux/slices/animeSlice";
+import favoriteReducer from "../redux/slices/favoriteSlice";
+import charactrerSlice from "../redux/slices/charactersSlice";
 
 export const store = configureStore({
   reducer: {
     anime: animeReducer,
-    favorite: favoriteReducer
+    favorite: favoriteReducer,
+    characters: charactrerSlice,
   },
 });
