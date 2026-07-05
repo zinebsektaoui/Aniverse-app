0d318847a274b6ebdb91969f330a9085ff554229
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-07-06 00:19:50 +0200

    notes and status slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import animeReducer from "../redux/slices/animeSlice";
 import favoriteReducer from "../redux/slices/favoriteSlice";
 import charactrerSlice from "../redux/slices/charactersSlice";
 import noteSlice from "./slices/noteSlice";
+import statusSlice from "./slices/statusSlice";
 
 export const store = configureStore({
   reducer: {
     anime: animeReducer,
     favorite: favoriteReducer,
     characters: charactrerSlice,
-    notes : noteSlice
+    notes : noteSlice,
+    status : statusSlice
   }
 });
